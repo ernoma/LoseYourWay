615df9dcd834ec573ab0a3885d5521eaa509107b
Erno Mäkinen <ernoma@gmail.com>
2016-04-04 16:22:28 +0300

    Prepare the map functionality to work in the offline mode

--- www/js/map-controllers.js
+++ www/js/map-controllers.js
@@ -9,10 +9,27 @@ var mapControllers = angular.module('mapControllers', [])
 		// attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	// }).addTo(map);
 	
+	var tilesDict = {
+		amsterdam: {
+			name: 'Amsterdam',
+			type: 'xyz',
+			url: '/data/maps/amsterdam/EPSG_900913_{z}/{x}_{y}.jpeg', 
+			options: {
+				tms: true,
+				maxNativeZoom: 17,
+				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			}
+		},	
+		openstreetmap: {
+			url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+			options: {
+				attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+			}
+		}
+	};
+	
 	$scope.map = {
           defaults: {
-            tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            maxZoom: 18,
             zoomControlPosition: 'bottomleft'
           },
           markers : {},
@@ -24,6 +41,10 @@ var mapControllers = angular.module('mapControllers', [])
           }
         };
 	
+	angular.extend($scope, {
+			tiles: tilesDict.openstreetmap
+	});
+	
 	$scope.goTo = function() {
 		$scope.map.center  = {
 			  lat : 61.5,
@@ -33,6 +54,24 @@ var mapControllers = angular.module('mapControllers', [])
 	}
 	
 	$scope.goTo();
+	
+	$scope.goToAmsterdam  = function() {
+		
+		$scope.tiles = tilesDict.amsterdam;
+		
+		$scope.map.center  = {
+			  lat : 52.3707,
+			  lng : 4.9004,
+			  zoom : 13
+			};
+			
+		//$scope.locate();
+	}
+	
+	$scope.showOnlineMap = function() {
+		$scope.tiles = tilesDict.openstreetmap;
+		$scope.locate();
+	}
 	
 	 $scope.locate = function(){
 
